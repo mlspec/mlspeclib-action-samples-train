35f4a1d56b826a7604dca1cc2a164fbf0f515359
David Aronchick <aronchick@gmail.com>
2020-05-27 22:34:16 +0000

    updating requirements

--- build_container.sh
+++ build_container.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+CONTAINER_NAME='mlspec/mlspeclib-action-samples-train'
+
 pipenv update mlspeclib
 pipenv lock -r > requirements.txt
 
@@ -6,5 +8,9 @@ git add .
 git commit -a -m 'updating requirements'
 git push
 
-docker build --no-cache -t mlspec/mlspeclib-action-samples-train .
-docker push mlspec/mlspeclib-action-samples-train
+docker build --no-cache -t $CONTAINER_NAME .
+python3 -m unittest tests/test*
+python3 -m unittest integration/test*
+
+docker push $CONTAINER_NAME
+
